ba9cb6bfbadb068e5c4e5f399913bfc3c98f5729
Juan David Gonzalez <juan.dagore@gmail.com>
2021-09-12 10:21:42 -0500

    Refactor function getFranchise add pattern to DB

--- app/Models/Franchise.php
+++ app/Models/Franchise.php
@@ -10,32 +10,21 @@ class Franchise extends Model
     use HasFactory;
 
     protected $table = 'franchises';
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'pattern'];
     public $timestamps = false;
+
+    static  $FRANCHISES = null;
 
     public static function getFranchise($num){
 
-        if(preg_match('/^3[47][0-9]{5,}$/', $num)){
-            $id = 1; //American Express
-        }
-        elseif (preg_match('/^3(?:0[0-5]|[68][0-9])[0-9]{4,}$/', $num)){
-            $id = 2; //Diners CLub
-        }
-        elseif (preg_match('/^6(?:011|5[0-9]{2})[0-9]{3,}$/', $num)){
-            $id = 3; //Discovery
-        }
-        elseif (preg_match('/^(?:2131|1800|35[0-9]{3})[0-9]{3,}$/', $num)){
-            $id = 4; //JCB
-        }
-        elseif (preg_match('/^5[1-5]/', $num)){
-            $id = 5; //MasterCard
-        }
-        elseif (preg_match('/^4[0-9]{6,}$/', $num)){
-            $id = 6; //Visa
-        }else{
-            return 1;
+        if(!self::$FRANCHISES){
+            self::$FRANCHISES = self::all();
         }
 
-        return $id;
+        foreach (self::$FRANCHISES as $franchise){
+            if(preg_match("/$franchise->pattern/", $num)){
+                return $franchise;
+            }
+        }
     }
 }
--- database/seeders/FranchiseSeeder.php
+++ database/seeders/FranchiseSeeder.php
@@ -16,26 +16,32 @@ class FranchiseSeeder extends Seeder
     {
         DB::table('franchises')->insert([
             'name' => 'American Express',
+            'pattern' => '^3[47][0-9]{5,}$'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'Diners Club',
+            'pattern' => '^3(?:0[0-5]|[68][0-9])[0-9]{4,}$'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'Discover',
+            'pattern' => '^6(?:011|5[0-9]{2})[0-9]{3,}$'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'JCB',
+            'pattern' => '^(?:2131|1800|35[0-9]{3})[0-9]{3,}$'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'MasterCard',
+            'pattern' => '^5[1-5]'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'Visa',
+            'pattern' => '^4[0-9]{6,}$'
         ]);
     }
 }
