ec1194431e5aa4228ff9d5b513f2f9a9b02e48e0
Juan David Gonzalez <juan.dagore@gmail.com>
2021-09-12 11:58:44 -0500

    Add min and max columns to franchises

--- database/seeders/FranchiseSeeder.php
+++ database/seeders/FranchiseSeeder.php
@@ -16,32 +16,44 @@ class FranchiseSeeder extends Seeder
     {
         DB::table('franchises')->insert([
             'name' => 'American Express',
-            'pattern' => '^3[47][0-9]{5,}$'
+            'pattern' => '^3[47][0-9]{5,}$',
+            'min' => '15',
+            'max' => '15'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'Diners Club',
-            'pattern' => '^3(?:0[0-5]|[68][0-9])[0-9]{4,}$'
+            'pattern' => '^3(?:0[0-5]|[68][0-9])[0-9]{4,}$',
+            'min' => '16',
+            'max' => '16'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'Discover',
-            'pattern' => '^6(?:011|5[0-9]{2})[0-9]{3,}$'
+            'pattern' => '^6(?:011|5[0-9]{2})[0-9]{3,}$',
+            'min' => '16',
+            'max' => '19'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'JCB',
-            'pattern' => '^(?:2131|1800|35[0-9]{3})[0-9]{3,}$'
+            'pattern' => '^(?:2131|1800|35[0-9]{3})[0-9]{3,}$',
+            'min' => '16',
+            'max' => '19'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'MasterCard',
-            'pattern' => '^5[1-5]'
+            'pattern' => '^5[1-5]',
+            'min' => '16',
+            'max' => '16'
         ]);
 
         DB::table('franchises')->insert([
             'name' => 'Visa',
-            'pattern' => '^4[0-9]{6,}$'
+            'pattern' => '^4[0-9]{6,}$',
+            'min' => '13',
+            'max' => '16'
         ]);
     }
 }
