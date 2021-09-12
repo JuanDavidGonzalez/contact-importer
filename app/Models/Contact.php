2224ad433e54b077d7b81786abc08d857b9fda62
Juan David Gonzalez <juan.dagore@gmail.com>
2021-09-11 22:38:43 -0500

    Create Franchise model & Update Contacts to add relation with franchises and add accessor for credit num

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -9,10 +9,23 @@ class Contact extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'birthday', 'phone', 'address', 'credit_card_number', 'email', 'user_id'];
+    protected $table = 'contacts';
+
+    protected $fillable = ['name', 'birthday', 'phone', 'address', 'credit_card_number', 'email', 'user_id', 'franchise_id', 'code'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function franchise()
+    {
+        return $this->belongsTo(Franchise::class);
+    }
+
+
+    public function getCardAttribute()
+    {
+        return "xxxx xxxx xxxx $this->code";
     }
 }
